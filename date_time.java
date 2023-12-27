3ac1a64f703131c326a0a2071364c32bcddcd6f8
Tenywa <samtenua@gmail.com>
2023-12-27 03:59:38 +0300

    Made new class

---
+++ date_time.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class date_time {
+
+public static void main(String[] args){
+LocalDateTime myDate = new LocalDateTime.now();
+System.out.println(myDate);
+}
+}
