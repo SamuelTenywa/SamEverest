9d5419827621a5c6ad6c19f63bbf8ecb4780a62e
SamuelTenywa <144793543+SamuelTenywa@users.noreply.github.com>
2023-12-28 23:09:28 +0300

    Update date_time.java

--- date_time.java
+++ date_time.java
@@ -1,9 +1,11 @@
 import java.time.LocalDateTime;
+import java.time.LocalDateTimeFormatter;
 import java.util.HashMap;
 public class date_time {
 
 public static void main(String[] args){
 LocalDateTime myDate = LocalDateTime.now();
+LocalDateTimeFormatter = myDate.now("HHH-mmm-sss mmm-ddd-yyy");
 System.out.println(myDate);
 HashMap<String,Character> chemicals = new HashMap<String,Character>();
 chemicals.put("Pottasium", 'K');
